fcef95d36b370d37f67082fbd1408341931a58f8
YAG-2122 <giapyoli@gmail.com>
2022-03-19 10:55:32 +0100

    Prueba1

--- Prueba1/Prueba1/Form1.cs
+++ Prueba1/Prueba1/Form1.cs
@@ -20,6 +20,7 @@ namespace Prueba1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("hola");
+            MessageBox.Show("T");
         }
     }
 }
